eea2795db53a5269c8eb8b0e62553c2af1b97840
Batmanninja1991 <109576315+Batmanninja1991@users.noreply.github.com>
2024-06-14 10:16:14 -0700

    fixed2?

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import mobile from "../assets/mobileBackground.webp";
 import desktop from "../assets/desktopBackground.webp";
-import banner from "../assets/nailSalonBanner.jpg";
 
 const Hero = () => {
   const [bgImage, setBgImage] = useState(mobile);
@@ -25,15 +24,16 @@ const Hero = () => {
   return (
     <div
       id="hero"
-      className="relative w-full h-screen flex justify-center items-center text-center pt-10 bg-center bg-fixed bg-cover"
+      className="relative w-full h-screen flex justify-center items-center text-center pt-10 bg-center bg-cover"
       style={{
+        backgroundAttachment: 'fixed',
         backgroundImage: `url(${bgImage})`,
       }}
     >
       <div className="absolute inset-0 bg-black bg-opacity-70"></div>
       <div className="w-11/12 md:w-2/3 lg:w-1/2 relative z-10 text-white flex flex-col p-4">
         <h1 className="text-5xl font-bold -translate-y-16 animate-fade-in-down">
-          Welcome to <br/>Mint Nails Studio
+          Welcome to <br />Mint Nails Studio
         </h1>
       </div>
     </div>
