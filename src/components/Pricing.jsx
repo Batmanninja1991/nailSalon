49e8eedeecf91a84e59d7d685674eb014975199d
Batmanninja1991 <109576315+Batmanninja1991@users.noreply.github.com>
2024-06-14 18:31:24 -0700

    edited prices

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -13,27 +13,18 @@ const Pricing = () => {
         <div className="w-11/12 lg:w-9/12 h-full bg-white rounded-3xl  shadow-gray-600 shadow-xl px-4 py-8">
           <ul className="grid grid-cols-2 py-1">
             <li className="text-xl pt-4 pb-4 pl-4 text-left">
-              Full Set
+              Nail Art Press Ons
+              <span className="text-sm"> ( includes charms and 3D art )</span>
               <div className="w-full border-b-2 border-black border-dotted"></div>
             </li>
             <li className="text-xl pt-4 pb-4 pr-4 pl-[1px] text-right">
-              $30 / $50
+              $70
               <div className="w-full border-b-2 border-black border-dotted"></div>
             </li>
           </ul>
           <ul className="grid grid-cols-2 py-1">
             <li className="text-xl pt-4 pb-4 pl-4 text-left">
-              Fill
-              <div className="w-full border-b-2 border-black border-dotted"></div>
-            </li>
-            <li className="text-xl pt-4 pb-4 pr-4 pl-[1px] text-right">
-              $40
-              <div className="w-full border-b-2 border-black border-dotted"></div>
-            </li>
-          </ul>
-          <ul className="grid grid-cols-2 py-1">
-            <li className="text-xl pt-4 pb-4 pl-4 text-left">
-              Dipping Full Set
+              French Tips
               <div className="w-full border-b-2 border-black border-dotted"></div>
             </li>
             <li className="text-xl pt-4 pb-4 pr-4 pl-[1px] text-right">
@@ -43,11 +34,21 @@ const Pricing = () => {
           </ul>
           <ul className="grid grid-cols-2 py-1">
             <li className="text-xl pt-4 pb-4 pl-4 text-left">
-              Custom Full Set
+              Solid Press Ons
               <div className="w-full border-b-2 border-black border-dotted"></div>
             </li>
             <li className="text-xl pt-4 pb-4 pr-4 pl-[1px] text-right">
-              $65
+              $40
+              <div className="w-full border-b-2 border-black border-dotted"></div>
+            </li>
+          </ul>
+          <ul className="grid grid-cols-2 py-1">
+            <li className="text-xl pt-4 pb-4 pl-4 text-left">
+              Sizing Kits
+              <div className="w-full border-b-2 border-black border-dotted"></div>
+            </li>
+            <li className="text-xl pt-4 pb-4 pr-4 pl-[1px] text-right">
+              $10
               <div className="w-full border-b-2 border-black border-dotted"></div>
             </li>
           </ul>
