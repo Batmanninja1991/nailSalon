ec21dc03793ec49c9fab360b32bb576a304e0c51
Batmanninja1991 <109576315+Batmanninja1991@users.noreply.github.com>
2024-06-19 15:25:00 -0700

    1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div
       id="hero"
-      className="relative w-full h-screen flex justify-center items-center text-center bg-cover bg-center bg-no-repeat pt-10"
+      className="relative w-screen h-screen flex justify-center items-center text-center bg-cover bg-center bg-no-repeat pt-10"
       style={{
         backgroundImage: `url(${bgImg})`,
       }}
